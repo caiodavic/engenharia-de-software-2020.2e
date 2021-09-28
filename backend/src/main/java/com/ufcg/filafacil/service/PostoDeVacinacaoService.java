b2280083474daa5c7c8786b7b57a890b3515e128
nicolasmnl <nicolas.leite@ccc.ufcg.edu>
2021-09-28 13:19:45 -0300

    Finalizei o cadastro e listagem de Postos de Vacinação

--- backend/src/main/java/com/ufcg/filafacil/service/PostoDeVacinacaoService.java
+++ backend/src/main/java/com/ufcg/filafacil/service/PostoDeVacinacaoService.java
@@ -3,7 +3,13 @@ package com.ufcg.filafacil.service;
 import com.ufcg.filafacil.DTO.PostoDeVacinacaoDTO;
 import com.ufcg.filafacil.model.posto_vacinacao.PostoDeVacinacao;
 
+import java.util.List;
+
 public interface PostoDeVacinacaoService {
+
     PostoDeVacinacao cadastraPostoVacinacao(PostoDeVacinacaoDTO PostoDTO);
-     PostoDeVacinacao getPostoById(long id);
+
+    PostoDeVacinacao getPostoById(long id);
+
+    List<PostoDeVacinacao> listar();
 }
--- backend/src/main/java/com/ufcg/filafacil/service/PostoDeVacinacaoServiceImpl.java
+++ backend/src/main/java/com/ufcg/filafacil/service/PostoDeVacinacaoServiceImpl.java
@@ -5,6 +5,7 @@ import com.ufcg.filafacil.model.posto_vacinacao.PostoDeVacinacao;
 import com.ufcg.filafacil.repository.PostoDeVacinacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 public class PostoDeVacinacaoServiceImpl implements PostoDeVacinacaoService{
@@ -15,11 +16,27 @@ public class PostoDeVacinacaoServiceImpl implements PostoDeVacinacaoService{
     @Override
     public PostoDeVacinacao cadastraPostoVacinacao(PostoDeVacinacaoDTO postoDTO) {
         Optional <PostoDeVacinacao> posto = postoRepository.findById(postoDTO.getId());
-        if(posto.isPresent()) throw new IllegalArgumentException("Id para Posto de Vacinação já utilizado")
+        if(posto.isPresent()) throw new IllegalArgumentException("Posto de Vacinação já cadastrado!");
+
+        return new PostoDeVacinacao(
+                postoDTO.getNome(),
+                postoDTO.getEmail(),
+                postoDTO.getTelefone(),
+                postoDTO.getEnderecoDTO().toEndereco(),
+                postoDTO.getId());
     }
 
     @Override
     public PostoDeVacinacao getPostoById(long id) {
-        return this.postoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Posto não cadastrado"));
+        return this.postoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Posto não cadastrado!"));
+    }
+
+    @Override
+    public List<PostoDeVacinacao> listar() {
+        List<PostoDeVacinacao> postos = postoRepository.findAll();
+        if(postos.isEmpty()){
+            throw new IllegalArgumentException("Não existem postos de vacinação cadastrados");
+        }
+        return postos;
     }
 }
