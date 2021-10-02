466d91021a1f8e5319659c39c4d1c2474f86c963
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2021-10-02 17:55:06 -0300

    fix: queue screen

--- frontend/src/pages/Queue/index.js
+++ frontend/src/pages/Queue/index.js
@@ -57,7 +57,7 @@ const Card = styled.div`
   flex-direction: column;
   border-radius: 20px;
   width: 500px;
-  height: 400px;
+  height: 250px;
   font-size: 20px;
   background-color: white;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
