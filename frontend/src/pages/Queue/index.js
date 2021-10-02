7782ca925e9e98bd0ebe041035814b8c141d9269
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2021-10-02 17:12:44 -0300

    add: inicio da tela

---
+++ frontend/src/pages/Queue/index.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import {
+  PageWrapper,
+  PageTitle,
+  PageSubTitle,
+  StyledForm,
+  PageContentContainer,
+} from '../../components/shared/CommonStyles';
+export default function Queue() {
+    useEffect(loadQueue,[]);
+function loadQueue(){
+}
+  return (
+    <PageWrapper>
+      <PageContentContainer>
+        <PageTitle>A sua senha para vacinação é</PageTitle>
+        <
+      </PageContentContainer>
+    </PageWrapper>
+  );
+}
